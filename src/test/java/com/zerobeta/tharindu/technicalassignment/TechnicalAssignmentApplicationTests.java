3e5ae7553a67677a3822a35b9d63cea21ce93665
Tharindu Gimras <75577550+Hedes-Productions@users.noreply.github.com>
2024-04-11 05:25:51 +0530

    Initialize Project

---
+++ src/test/java/com/zerobeta/tharindu/technicalassignment/TechnicalAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobeta.tharindu.technicalassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
