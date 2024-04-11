46fc7c766e290f27d3f9364491e141417cb72491
Tharindu Gimras <75577550+Hedes-Productions@users.noreply.github.com>
2024-04-11 06:32:15 +0530

    Add initial orders and users

--- src/main/java/com/zerobeta/tharindu/technicalassignment/TechnicalAssignmentApplication.java
+++ src/main/java/com/zerobeta/tharindu/technicalassignment/TechnicalAssignmentApplication.java
@@ -1,7 +1,13 @@
 package com.zerobeta.tharindu.technicalassignment;
 
+import com.zerobeta.tharindu.technicalassignment.model.Order;
+import com.zerobeta.tharindu.technicalassignment.model.User;
+import com.zerobeta.tharindu.technicalassignment.repository.OrderRepository;
+import com.zerobeta.tharindu.technicalassignment.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TechnicalAssignmentApplication {
@@ -10,4 +16,24 @@ public class TechnicalAssignmentApplication {
 		SpringApplication.run(TechnicalAssignmentApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(UserRepository users, OrderRepository orders){
+		return args -> {
+			users.save(User.builder()
+					.email("tharindu@gmail.com")
+					.password("password")
+					.firstName("tharindu")
+					.lastName("gimras")
+					.build()
+			);
+
+			orders.save(Order.builder()
+					.item_name("tharindu")
+					.quantity(5)
+					.address("70/4,Web,Nit")
+					.status("fine")
+					.build()
+			);
+		};
+	}
 }
