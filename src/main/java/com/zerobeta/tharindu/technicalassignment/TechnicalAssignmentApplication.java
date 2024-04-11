a6ff778afb86bf8db2712e1663ef3fc77f204611
Tharindu Gimras <75577550+Hedes-Productions@users.noreply.github.com>
2024-04-12 05:28:33 +0530

    Modify by adding an initial user with orders in CommandLineRunner for testing purposes

--- src/main/java/com/zerobeta/tharindu/technicalassignment/TechnicalAssignmentApplication.java
+++ src/main/java/com/zerobeta/tharindu/technicalassignment/TechnicalAssignmentApplication.java
@@ -2,12 +2,15 @@ package com.zerobeta.tharindu.technicalassignment;
 
 import com.zerobeta.tharindu.technicalassignment.model.Order;
 import com.zerobeta.tharindu.technicalassignment.model.User;
-import com.zerobeta.tharindu.technicalassignment.repository.OrderRepository;
 import com.zerobeta.tharindu.technicalassignment.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class TechnicalAssignmentApplication {
@@ -17,21 +20,29 @@ public class TechnicalAssignmentApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(UserRepository users, OrderRepository orders){
+	public CommandLineRunner commandLineRunner(UserRepository users, PasswordEncoder passwordEncoder){
 		return args -> {
-			users.save(User.builder()
-					.email("tharindu@gmail.com")
-					.password("password")
-					.firstName("tharindu")
-					.lastName("gimras")
-					.build()
-			);
-
-			orders.save(Order.builder()
-					.item_name("tharindu")
+			Set<Order> orders = new HashSet<>();
+			orders.add(Order.builder()
+					.item_name("dhal")
 					.quantity(5)
 					.address("70/4,Web,Nit")
 					.status("fine")
+					.build()
+			);
+			orders.add(Order.builder()
+					.item_name("carrot")
+					.quantity(10)
+					.address("70/4,Web,Nit")
+					.status("fine")
+					.build()
+			);
+			users.save(User.builder()
+					.email("tharindu@gmail.com")
+					.password(passwordEncoder.encode("password"))
+					.firstName("tharindu")
+					.lastName("gimras")
+					.orders(orders)
 					.build()
 			);
 		};
