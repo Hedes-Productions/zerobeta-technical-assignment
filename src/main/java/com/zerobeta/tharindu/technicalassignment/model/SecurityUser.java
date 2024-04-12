c28bc509615b416e17561cdf7182422f5dff835e
Tharindu Gimras <75577550+Hedes-Productions@users.noreply.github.com>
2024-04-12 15:22:11 +0530

    Modify SecurityUser decorators

--- src/main/java/com/zerobeta/tharindu/technicalassignment/model/SecurityUser.java
+++ src/main/java/com/zerobeta/tharindu/technicalassignment/model/SecurityUser.java
@@ -1,14 +1,14 @@
 package com.zerobeta.tharindu.technicalassignment.model;
 
 import lombok.Builder;
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
-@RequiredArgsConstructor
 @Builder
+@Getter
 public class SecurityUser implements UserDetails {
     private final Long userId;
     private final String email;
