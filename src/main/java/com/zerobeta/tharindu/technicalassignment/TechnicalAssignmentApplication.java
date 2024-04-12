879ded439d252a57e6401e1fa13e58dc6cb384a5
Tharindu Gimras <75577550+Hedes-Productions@users.noreply.github.com>
2024-04-13 00:19:31 +0530

    Remove initial commandline runner user creation

--- src/main/java/com/zerobeta/tharindu/technicalassignment/TechnicalAssignmentApplication.java
+++ src/main/java/com/zerobeta/tharindu/technicalassignment/TechnicalAssignmentApplication.java
@@ -18,33 +18,4 @@ public class TechnicalAssignmentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TechnicalAssignmentApplication.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(UserRepository users, PasswordEncoder passwordEncoder){
-		return args -> {
-			Set<Order> orders = new HashSet<>();
-			orders.add(Order.builder()
-					.item_name("dhal")
-					.quantity(5)
-					.address("70/4,Web,Nit")
-					.status("fine")
-					.build()
-			);
-			orders.add(Order.builder()
-					.item_name("carrot")
-					.quantity(10)
-					.address("70/4,Web,Nit")
-					.status("fine")
-					.build()
-			);
-			users.save(User.builder()
-					.email("tharindu@gmail.com")
-					.password(passwordEncoder.encode("password"))
-					.firstName("tharindu")
-					.lastName("gimras")
-					.orders(orders)
-					.build()
-			);
-		};
-	}
 }
