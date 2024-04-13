b8b8fe75084d2f1264293f2b1e63f51d15a1c8ac
Tharindu Gimras <75577550+Hedes-Productions@users.noreply.github.com>
2024-04-13 06:42:40 +0530

    Add functionality of 30 seconds time interval order dispatching

---
+++ src/main/java/com/zerobeta/tharindu/technicalassignment/job/OrderDispatchJob.java
@@ -0,0 +1,27 @@
+package com.zerobeta.tharindu.technicalassignment.job;
+
+import com.zerobeta.tharindu.technicalassignment.model.Order;
+import com.zerobeta.tharindu.technicalassignment.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RequiredArgsConstructor
+@Component
+public class OrderDispatchJob {
+    private final OrderRepository orderRepository;
+
+    @Scheduled(fixedRate = 1000*30)
+    public void dispatchOrders() {
+        System.out.println("Running scheduled task...");
+        List<Order> newOrders = orderRepository.findAllByStatus("NEW").orElseThrow(()->new NoSuchElementException("No new orders"));
+        for (Order order: newOrders){
+            order.setStatus("DISPATCHED");
+        }
+        orderRepository.saveAll(newOrders);
+        System.out.println("Dispatch job was executed.");
+    }
+}
--- src/main/java/com/zerobeta/tharindu/technicalassignment/repository/OrderRepository.java
+++ src/main/java/com/zerobeta/tharindu/technicalassignment/repository/OrderRepository.java
@@ -4,8 +4,11 @@ import com.zerobeta.tharindu.technicalassignment.model.Order;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface OrderRepository extends CrudRepository<Order, UUID> {
+    Optional<List<Order>> findAllByStatus(String status);
 }
