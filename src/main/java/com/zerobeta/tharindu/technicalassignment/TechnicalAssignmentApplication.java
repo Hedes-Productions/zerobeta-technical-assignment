bceb9330c9a674e3fa388eb7f9545ac440610867
Tharindu Gimras <75577550+Hedes-Productions@users.noreply.github.com>
2024-04-14 01:14:25 +0530

    Remove unused imports

--- src/main/java/com/zerobeta/tharindu/technicalassignment/TechnicalAssignmentApplication.java
+++ src/main/java/com/zerobeta/tharindu/technicalassignment/TechnicalAssignmentApplication.java
@@ -1,18 +1,8 @@
 package com.zerobeta.tharindu.technicalassignment;
 
-import com.zerobeta.tharindu.technicalassignment.model.Order;
-import com.zerobeta.tharindu.technicalassignment.model.User;
-import com.zerobeta.tharindu.technicalassignment.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.HashSet;
-import java.util.Set;
-
 @SpringBootApplication
 @EnableScheduling
 public class TechnicalAssignmentApplication {
