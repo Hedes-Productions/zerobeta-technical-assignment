d8d2b33a1fab424c818fcb826a1236afdc6dd070
Tharindu Gimras <75577550+Hedes-Productions@users.noreply.github.com>
2024-04-14 01:12:15 +0530

    Get Jwt secret from application.properties

--- src/main/java/com/zerobeta/tharindu/technicalassignment/utils/JwtIssuer.java
+++ src/main/java/com/zerobeta/tharindu/technicalassignment/utils/JwtIssuer.java
@@ -2,6 +2,7 @@ package com.zerobeta.tharindu.technicalassignment.utils;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
@@ -9,13 +10,15 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
 @Component
+@RequiredArgsConstructor
 public class JwtIssuer {
+    private final JwtProperties jwtProperties;
     public String issue(Long userId, String email){
         return JWT.create()
                 .withSubject(String.valueOf(userId))
                 .withClaim("email",email)
                 .withExpiresAt(Instant.now().plus(Duration.of(1, ChronoUnit.DAYS)))
-                .sign(Algorithm.HMAC256("secret"));
+                .sign(Algorithm.HMAC256(jwtProperties.getSecretKey()));
     }
 
 }
