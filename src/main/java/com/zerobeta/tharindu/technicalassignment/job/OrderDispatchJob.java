935561824fc649796af3a11a1cec9fb8a1d897e6
Tharindu Gimras <75577550+Hedes-Productions@users.noreply.github.com>
2024-04-13 06:45:10 +0530

    Add a custom logger for order dispatching

--- src/main/java/com/zerobeta/tharindu/technicalassignment/job/OrderDispatchJob.java
+++ src/main/java/com/zerobeta/tharindu/technicalassignment/job/OrderDispatchJob.java
@@ -3,6 +3,8 @@ package com.zerobeta.tharindu.technicalassignment.job;
 import com.zerobeta.tharindu.technicalassignment.model.Order;
 import com.zerobeta.tharindu.technicalassignment.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,15 +15,16 @@ import java.util.NoSuchElementException;
 @Component
 public class OrderDispatchJob {
     private final OrderRepository orderRepository;
+    private final Logger logger = LoggerFactory.getLogger("OrderDispatch");
 
     @Scheduled(fixedRate = 1000*30)
     public void dispatchOrders() {
-        System.out.println("Running scheduled task...");
+        logger.info("Running scheduled task...");
         List<Order> newOrders = orderRepository.findAllByStatus("NEW").orElseThrow(()->new NoSuchElementException("No new orders"));
         for (Order order: newOrders){
             order.setStatus("DISPATCHED");
         }
         orderRepository.saveAll(newOrders);
-        System.out.println("Dispatch job was executed.");
+        logger.info("Dispatch job was executed.");
     }
 }
